2cbdb8eedcfc94ffb523cdb8c482150f928e45d3
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-01-01 11:49:44 -0300

    create page Request

--- src/pages/Request/index.tsx
+++ src/pages/Request/index.tsx
@@ -10,90 +10,139 @@ import {
   Trash,
 } from 'phosphor-react'
 import { RequestContainer } from './styles'
+import { useState } from 'react'
 
 export function Request() {
+  const [click, setClick] = useState(true)
+
+  function handleClick() {
+    if (click === true) {
+      setClick(false)
+    } else {
+      setClick(true)
+    }
+  }
+
   return (
     <RequestContainer>
-      <div className="enterData">
-        <h2>Complete seu pedido</h2>
-        <div className="containerData">
-          <div className="informations">
-            <MapPinLine size={22} color="#C47F17" />
-            <div className="Informations">
-              <h3>Endereço de entrega</h3>
-              <p>Informe o endereço onde deseja receber seu pedido</p>
+      {click ? (
+        <main>
+          <div className="enterData">
+            <h2>Complete seu pedido</h2>
+            <div className="containerData">
+              <div className="informations">
+                <MapPinLine size={22} color="#C47F17" />
+                <div className="Informations">
+                  <h3>Endereço de entrega</h3>
+                  <p>Informe o endereço onde deseja receber seu pedido</p>
+                </div>
+              </div>
+              <input className="cep" type="text" placeholder="CEP" />
+              <input className="street" type="text" placeholder="Rua" />
+              <div className="numberAndComplement">
+                <input className="number" type="text" placeholder="Número" />
+                <input
+                  className="complement"
+                  type="text"
+                  placeholder="Complemento                                                   Opcional"
+                />
+              </div>
+              <div className="address">
+                <input className="district" type="text" placeholder="Bairro" />
+                <input className="city" type="text" placeholder="Cidade" />
+                <input className="state" type="text" placeholder="UF" />
+              </div>
+            </div>
+            <div className="payment">
+              <div className="paymentContainer">
+                <CurrencyDollar size={22} color="#8047F8" />
+                <div className="paymentContainerTwo">
+                  <h3>Pagamento</h3>
+                  <p>
+                    O pagamento é feito na entrega. Escolha a forma que deseja
+                    pagar
+                  </p>
+                </div>
+              </div>
+              <div className="formOfPayment">
+                <button className="credit">
+                  <CreditCard size={16} color="#8047F8" /> CARTÃO DE CRÉDITO
+                </button>
+                <button className="debit">
+                  <Bank size={16} color="#8047F8" /> CARTÃO DE DÉBITO
+                </button>
+                <button className="cash">
+                  <Money size={16} color="#8047F8" /> DINHEIRO
+                </button>
+              </div>
             </div>
           </div>
-          <input className="cep" type="text" placeholder="CEP" />
-          <input className="street" type="text" placeholder="Rua" />
-          <div className="numberAndComplement">
-            <input className="number" type="text" placeholder="Número" />
-            <input
-              className="complement"
-              type="text"
-              placeholder="Complemento                                                   Opcional"
-            />
-          </div>
-          <div className="address">
-            <input className="district" type="text" placeholder="Bairro" />
-            <input className="city" type="text" placeholder="Cidade" />
-            <input className="state" type="text" placeholder="UF" />
-          </div>
-        </div>
-        <div className="payment">
-          <div className="paymentContainer">
-            <CurrencyDollar size={22} color="#8047F8" />
-            <div className="paymentContainerTwo">
-              <h3>Pagamento</h3>
-              <p>
-                O pagamento é feito na entrega. Escolha a forma que deseja pagar
-              </p>
-            </div>
-          </div>
-          <div className="formOfPayment">
-            <button className="credit">
-              <CreditCard size={16} color="#8047F8" /> CARTÃO DE CRÉDITO
-            </button>
-            <button className="debit">
-              <Bank size={16} color="#8047F8" /> CARTÃO DE DÉBITO
-            </button>
-            <button className="cash">
-              <Money size={16} color="#8047F8" /> DINHEIRO
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="selectedCoffees">
-        <h2>Cafés selecionados</h2>
-        <div className="ContainerCoffees">
-          <div className="coffee">
-            <img src={CoffeeOne} alt="imagem do café" />
-            <div className="content">
-              <p>Expresso Tradicional</p>
-              <span>
-                <Minus /> 1 <Plus />
-              </span>
-              <button>
-                <Trash size={16} color="#8047F8" /> REMOVER
+          <div className="selectedCoffees">
+            <h2>Cafés selecionados</h2>
+            <div className="ContainerCoffees">
+              <div className="coffee">
+                <img src={CoffeeOne} alt="imagem do café" />
+                <div className="content">
+                  <p>Expresso Tradicional</p>
+                  <div className="amountAndButton">
+                    <span className="amountItem">
+                      <button>
+                        <Minus color="#8047F8" weight="fill" />
+                      </button>
+                      1
+                      <button>
+                        <Plus color="#8047F8" weight="fill" />
+                      </button>
+                    </span>
+                    <button className="button">
+                      <Trash size={16} color="#8047F8" /> REMOVER
+                    </button>
+                  </div>
+                </div>
+                <span className="price">R$ 9,90</span>
+              </div>
+              <div className="coffee">
+                <img src={CoffeeOne} alt="imagem do café" />
+                <div className="content">
+                  <p>Expresso Tradicional</p>
+                  <div className="amountAndButton">
+                    <span className="amountItem">
+                      <button>
+                        <Minus color="#8047F8" weight="fill" />
+                      </button>
+                      1
+                      <button>
+                        <Plus color="#8047F8" weight="fill" />
+                      </button>
+                    </span>
+                    <button className="button">
+                      <Trash size={16} color="#8047F8" /> REMOVER
+                    </button>
+                  </div>
+                </div>
+                <span className="price">R$ 9,90</span>
+              </div>
+              <div className="fullItems">
+                <span>Total de itens</span>
+                <span>R$ 29,70</span>
+              </div>
+              <div className="delivery">
+                <span>Entrega</span>
+                <span>R$ 3,50</span>
+              </div>
+              <div className="fullPrice">
+                <span>Total</span>
+                <span>R$ 33,20</span>
+              </div>
+              <button onClick={handleClick} className="confirmRequest">
+                CONFIRMAR PEDIDO
               </button>
             </div>
-            <span>R$ 9,90</span>
           </div>
-        </div>
-        <div className="fullItems">
-          <span>Total de itens</span>
-          <span>R$ 29,70</span>
-        </div>
-        <div className="delivery">
-          <span>Entrega</span>
-          <span>R$ 3,50</span>
-        </div>
-        <div className="fullPrice">
-          <span>Total</span>
-          <span>R$ 33,20</span>
-        </div>
-        <button className="confirmRequest">CONFIRMAR PEDIDO</button>
-      </div>
+        </main>
+      ) : (
+        <h1>Hello world!</h1>
+      )}
     </RequestContainer>
   )
 }
--- src/pages/Request/styles.ts
+++ src/pages/Request/styles.ts
@@ -3,9 +3,30 @@ import styled from 'styled-components'
 export const RequestContainer = styled.div`
   height: 100vh;
   overflow-y: auto;
-  display: flex;
-  gap: 2rem;
-  padding: 0 10rem;
+  &::-webkit-scrollbar {
+    width: 15px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme['purple-200']};
+    border-radius: 10px;
+    width: 0px;
+    background-clip: padding-box;
+    border: 3px solid transparent;
+  }
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: ${(props) => props.theme['purple-300']};
+    border-radius: 0px;
+    width: 0px;
+    border-radius: 10px;
+    background-clip: padding-box;
+    border: 3px solid transparent;
+  }
+  main {
+    display: flex;
+    justify-content: space-between;
+    gap: 2rem;
+    padding: 0 10rem;
+  }
   h2 {
     font-family: 'Baloo 2';
     font-weight: 700;
@@ -28,6 +49,10 @@ export const RequestContainer = styled.div`
       line-height: 1.3;
       color: ${(props) => props.theme['gray-700']};
     }
+  }
+  .enterData {
+    height: 100vh;
+    padding-bottom: 5rem;
   }
   .containerData {
     display: flex;
@@ -93,7 +118,6 @@ export const RequestContainer = styled.div`
     width: 40rem;
     background: ${(props) => props.theme['gray-300']};
     border-radius: 6px;
-    margin-bottom: 0.75rem;
     .paymentContainer {
       display: flex;
       align-items: flex-start;
@@ -142,6 +166,160 @@ export const RequestContainer = styled.div`
         &:hover {
           background: ${(props) => props.theme['gray-600']};
         }
+      }
+    }
+  }
+  .ContainerCoffees {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2.5rem;
+    width: 28rem;
+    background: ${(props) => props.theme['gray-300']};
+    border-radius: 6px 44px 6px 44px;
+    .coffee {
+      display: flex;
+      align-items: flex-start;
+      padding-bottom: 1.5rem;
+      padding-top: 1.5rem;
+      border-bottom: 1px solid ${(props) => props.theme['gray-500']};
+      &:nth-child(1) {
+        padding-bottom: 1.5rem;
+        padding-top: 0;
+      }
+      img {
+        width: 4rem;
+        height: 4rem;
+      }
+      .content {
+        margin-left: 1.25rem;
+        margin-right: 3.125rem;
+        p {
+          font-weight: 400;
+          font-size: 1rem;
+          line-height: 1.3;
+          color: ${(props) => props.theme['brown-200']};
+          margin-bottom: 0.5rem;
+        }
+        .amountAndButton {
+          display: flex;
+          align-items: center;
+          gap: 0.5rem;
+        }
+        .amountItem {
+          button {
+            background: none;
+            border: none;
+            cursor: pointer;
+          }
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 0.25rem;
+          width: 4.5rem;
+          height: 2rem;
+          background: ${(props) => props.theme['gray-500']};
+          border-radius: 6px;
+        }
+        .button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          gap: 0.25rem;
+          width: 5.6875rem;
+          height: 2rem;
+          background: ${(props) => props.theme['gray-500']};
+          border-radius: 6px;
+          font-weight: 400;
+          font-size: 0.75rem;
+          line-height: 1.6;
+          color: ${(props) => props.theme['brown-100']};
+          border: none;
+          cursor: pointer;
+          &:hover {
+            background: ${(props) => props.theme['gray-600']};
+          }
+        }
+      }
+      .price {
+        font-weight: 700;
+        font-size: 1rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+    }
+    .fullItems {
+      margin-top: 1.5625rem;
+      margin-bottom: 0.75rem;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      span:nth-child(1) {
+        font-size: 0.875rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+      span:nth-child(2) {
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+    }
+    .delivery {
+      margin-bottom: 0.75rem;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      span:nth-child(1) {
+        font-size: 0.875rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+      span:nth-child(2) {
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+    }
+    .fullPrice {
+      margin-bottom: 1.5rem;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      span:nth-child(1) {
+        font-size: 0.875rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+      span:nth-child(2) {
+        font-weight: 400;
+        font-size: 1rem;
+        line-height: 1.3;
+        color: ${(props) => props.theme['brown-100']};
+      }
+    }
+    .confirmRequest {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border: none;
+      gap: 0.25rem;
+      width: 100%;
+      height: 2.875rem;
+      background: ${(props) => props.theme['yellow-200']};
+      border-radius: 6px;
+      font-weight: 700;
+      font-size: 0.875rem;
+      line-height: 1.6;
+      color: ${(props) => props.theme.white};
+      cursor: pointer;
+      &:hover {
+        background: ${(props) => props.theme['yellow-300']};
       }
     }
   }
